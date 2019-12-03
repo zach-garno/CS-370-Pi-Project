55b234a245162ee8995566779c12cdcc92025033
Austin Soriano <asori@mabasori.com>
2019-12-03 11:35:16 -0700

    added REST api stuff

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
-def main():
+from flask import json, request, Flask
 
+
+def firePushAction():
+    print("Push Happened!")
+
+def fireStarAction():
+    print("Star Happened!")
+
+def handleJson(gitJson):
+    if "pusher" in gitJson:
+        firePushAction()
+    elif "starred_at" in gitJson:
+        fireStarAction()
     pass
 
+def fireLocalCommit():
+    print("Local Commit Happened!")
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return "welcome"
+
+@app.route('/local', methods=['POST'])
+def local_commit():
+    fireLocalCommit()
+    return "OK"
+
+@app.route('/github', methods=['POST'])
+def github_message():
+    if request.headers['Content-Type'] == 'application/json':
+        # print(json.dumps(request.json, indent=4, sort_keys=True))
+        handleJson(request.json)
+        return json.dumps(request.json, indent=4, sort_keys=True)
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
+
+
+
