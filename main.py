5dc2ce66c35176c65c9d97823f143a5c62ba3918
Austin Soriano <asori@mabasori.com>
2019-12-05 17:36:33 -0700

    add start and stop server

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from time import sleep
 from flask import json, request, Flask
 import os
+from multiprocessing import Process
+
 print(os.uname()[1])
-if (os.uname()[1] == 'raspberrypi'):
+if os.uname()[1] == 'raspberrypi':
     import RPi.GPIO as GPIO
 
 # pins
@@ -10,31 +12,37 @@ motorPin = 25
 commitPin = 24
 starPin = 23
 
+server = None
+
+
 def setup():
-    if (os.uname()[1] == 'raspberrypi'):
+    if os.uname()[1] == 'raspberrypi':
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(motorPin, GPIO.OUT)
         GPIO.setup(commitPin, GPIO.OUT)
         GPIO.setup(starPin, GPIO.OUT)
 
+
 def firePushAction():
     print("Push Happened!")
-    if (os.uname()[1] == 'raspberrypi'):
+    if os.uname()[1] == 'raspberrypi':
         GPIO.output(motorPin, GPIO.HIGH)
         GPIO.output(commitPin, GPIO.LOW)
         GPIO.output(starPin, GPIO.HIGH)
         sleep(10)
         GPIO.cleanup()
 
+
 def fireStarAction():
     print("Star Happened!")
-    if (os.uname()[1] == 'raspberrypi'):
+    if os.uname()[1] == 'raspberrypi':
         GPIO.output(starPin, GPIO.HIGH)
         GPIO.output(starPin, GPIO.LOW)
         GPIO.output(starPin, GPIO.HIGH)
         GPIO.output(starPin, GPIO.LOW)
         GPIO.output(starPin, GPIO.HIGH)
         GPIO.output(starPin, GPIO.LOW)
+
 
 def handleJson(gitJson):
     if "pusher" in gitJson:
@@ -43,9 +51,10 @@ def handleJson(gitJson):
         fireStarAction()
     pass
 
+
 def fireLocalCommit():
     print("Local Commit Happened!")
-    if (os.uname()[1] == 'raspberrypi'):
+    if os.uname()[1] == 'raspberrypi':
         GPIO.output(commitPin, GPIO.HIGH)
         GPIO.output(commitPin, GPIO.LOW)
         GPIO.output(commitPin, GPIO.HIGH)
@@ -53,16 +62,20 @@ def fireLocalCommit():
         GPIO.output(commitPin, GPIO.HIGH)
         GPIO.output(commitPin, GPIO.LOW)
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def api_root():
     return "welcome"
 
+
 @app.route('/local', methods=['POST'])
 def local_commit():
     fireLocalCommit()
     return "OK"
+
 
 @app.route('/github', methods=['POST'])
 def github_message():
@@ -71,9 +84,21 @@ def github_message():
         handleJson(request.json)
         return json.dumps(request.json, indent=4, sort_keys=True)
 
+
+def startServer():
+    if server is not None:
+        server.start()
+    else:
+        print("server does not exist!")
+
+
+def stopServer():
+    if server is not None:
+        server.terminate()
+    else:
+        print("server does not exist!")
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    server = Process(app.run(debug=True))
     setup()
-
-
-
